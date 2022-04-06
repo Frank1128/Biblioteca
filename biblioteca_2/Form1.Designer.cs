a454217c0eab591a25147f9429e4c8f846a5e759
Frank11 <fkamilo11@hotmail.com>
2022-04-05 19:39:37 -0500

    cambios

---
+++ biblioteca_2/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace biblioteca_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.txtautor1 = new System.Windows.Forms.TextBox();
+            this.txtlibro1 = new System.Windows.Forms.TextBox();
+            this.txtubicacion = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtdisponible = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button1.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(813, 33);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(124, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button2.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(813, 148);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 46);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Buscar";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button3.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(108, 461);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(174, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Nueva Busqueda";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.button4.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(771, 481);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(124, 55);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "salir";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // txtautor1
+            // 
+            this.txtautor1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtautor1.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtautor1.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtautor1.Location = new System.Drawing.Point(476, 33);
+            this.txtautor1.Multiline = true;
+            this.txtautor1.Name = "txtautor1";
+            this.txtautor1.ReadOnly = true;
+            this.txtautor1.Size = new System.Drawing.Size(262, 44);
+            this.txtautor1.TabIndex = 4;
+            this.txtautor1.TabStop = false;
+            this.txtautor1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // txtlibro1
+            // 
+            this.txtlibro1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtlibro1.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtlibro1.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtlibro1.Location = new System.Drawing.Point(537, 148);
+            this.txtlibro1.Multiline = true;
+            this.txtlibro1.Name = "txtlibro1";
+            this.txtlibro1.ReadOnly = true;
+            this.txtlibro1.Size = new System.Drawing.Size(201, 42);
+            this.txtlibro1.TabIndex = 5;
+            this.txtlibro1.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtubicacion
+            // 
+            this.txtubicacion.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtubicacion.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtubicacion.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtubicacion.Location = new System.Drawing.Point(537, 231);
+            this.txtubicacion.Multiline = true;
+            this.txtubicacion.Name = "txtubicacion";
+            this.txtubicacion.ReadOnly = true;
+            this.txtubicacion.Size = new System.Drawing.Size(201, 44);
+            this.txtubicacion.TabIndex = 6;
+            this.txtubicacion.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(271, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 30);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Autor";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(271, 152);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(193, 30);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Nombre del Libro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(271, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(111, 30);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Ubicacion";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(271, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(119, 30);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Disponible";
+            // 
+            // txtdisponible
+            // 
+            this.txtdisponible.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.txtdisponible.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtdisponible.Font = new System.Drawing.Font("Comic Sans MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtdisponible.Location = new System.Drawing.Point(590, 333);
+            this.txtdisponible.Multiline = true;
+            this.txtdisponible.Name = "txtdisponible";
+            this.txtdisponible.ReadOnly = true;
+            this.txtdisponible.ShortcutsEnabled = false;
+            this.txtdisponible.Size = new System.Drawing.Size(148, 39);
+            this.txtdisponible.TabIndex = 11;
+            this.txtdisponible.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.BackgroundImage = global::biblioteca_2.Properties.Resources.fondo;
+            this.ClientSize = new System.Drawing.Size(995, 593);
+            this.Controls.Add(this.txtdisponible);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtubicacion);
+            this.Controls.Add(this.txtlibro1);
+            this.Controls.Add(this.txtautor1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Biblioteca Nacional";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox txtautor1;
+        private System.Windows.Forms.TextBox txtlibro1;
+        private System.Windows.Forms.TextBox txtubicacion;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtdisponible;
+    }
+}
+
