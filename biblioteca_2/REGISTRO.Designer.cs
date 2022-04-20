358df029b89b285fe4761107215ceb2e4df8cbf5
Frank11 <fkamilo11@hotmail.com>
2022-04-19 21:00:29 -0500

    Activando el reggistro

--- biblioteca_2/REGISTRO.Designer.cs
+++ biblioteca_2/REGISTRO.Designer.cs
@@ -28,160 +28,146 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btn_Registrarse = new System.Windows.Forms.Button();
+            this.txt_nombre = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox4 = new System.Windows.Forms.TextBox();
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
+            this.txt_tel = new System.Windows.Forms.Label();
+            this.txt_email = new System.Windows.Forms.Label();
+            this.txt_apellido = new System.Windows.Forms.Label();
+            this.txt_cc = new System.Windows.Forms.Label();
+            this.txt_contraseña = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // btn_Registrarse
             // 
-            this.button1.Location = new System.Drawing.Point(155, 327);
-            this.button1.Margin = new System.Windows.Forms.Padding(2);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(121, 39);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "REGISTRARSE";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.btn_Registrarse.Location = new System.Drawing.Point(232, 503);
+            this.btn_Registrarse.Name = "btn_Registrarse";
+            this.btn_Registrarse.Size = new System.Drawing.Size(182, 60);
+            this.btn_Registrarse.TabIndex = 0;
+            this.btn_Registrarse.Text = "REGISTRARSE";
+            this.btn_Registrarse.UseVisualStyleBackColor = true;
+            this.btn_Registrarse.Click += new System.EventHandler(this.btn_registro);
             // 
-            // textBox1
+            // txt_nombre
             // 
-            this.textBox1.Location = new System.Drawing.Point(239, 27);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(102, 20);
-            this.textBox1.TabIndex = 1;
+            this.txt_nombre.Location = new System.Drawing.Point(358, 42);
+            this.txt_nombre.Name = "txt_nombre";
+            this.txt_nombre.Size = new System.Drawing.Size(151, 26);
+            this.txt_nombre.TabIndex = 1;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(239, 172);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox2.Location = new System.Drawing.Point(358, 265);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(102, 20);
+            this.textBox2.Size = new System.Drawing.Size(151, 26);
             this.textBox2.TabIndex = 2;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(239, 218);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox3.Location = new System.Drawing.Point(358, 335);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(102, 20);
+            this.textBox3.Size = new System.Drawing.Size(151, 26);
             this.textBox3.TabIndex = 3;
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(239, 86);
-            this.textBox4.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox4.Location = new System.Drawing.Point(358, 132);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(102, 20);
+            this.textBox4.Size = new System.Drawing.Size(151, 26);
             this.textBox4.TabIndex = 4;
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(239, 133);
-            this.textBox5.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox5.Location = new System.Drawing.Point(358, 205);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(102, 20);
+            this.textBox5.Size = new System.Drawing.Size(151, 26);
             this.textBox5.TabIndex = 5;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(239, 264);
-            this.textBox6.Margin = new System.Windows.Forms.Padding(2);
+            this.textBox6.Location = new System.Drawing.Point(358, 406);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(102, 20);
+            this.textBox6.Size = new System.Drawing.Size(151, 26);
             this.textBox6.TabIndex = 6;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(140, 34);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(210, 52);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.Size = new System.Drawing.Size(79, 20);
             this.label1.TabIndex = 7;
             this.label1.Text = "NOMBRE";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // txt_tel
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(130, 175);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "TELEFONO";
+            this.txt_tel.AutoSize = true;
+            this.txt_tel.Location = new System.Drawing.Point(195, 269);
+            this.txt_tel.Name = "txt_tel";
+            this.txt_tel.Size = new System.Drawing.Size(94, 20);
+            this.txt_tel.TabIndex = 8;
+            this.txt_tel.Text = "TELEFONO";
             // 
-            // label3
+            // txt_email
             // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(152, 218);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(42, 13);
-            this.label3.TabIndex = 9;
-            this.label3.Text = "E-MAIL";
+            this.txt_email.AutoSize = true;
+            this.txt_email.Location = new System.Drawing.Point(228, 335);
+            this.txt_email.Name = "txt_email";
+            this.txt_email.Size = new System.Drawing.Size(63, 20);
+            this.txt_email.TabIndex = 9;
+            this.txt_email.Text = "E-MAIL";
             // 
-            // label4
+            // txt_apellido
             // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(135, 89);
-            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 13);
-            this.label4.TabIndex = 10;
-            this.label4.Text = "APELLIDO";
+            this.txt_apellido.AutoSize = true;
+            this.txt_apellido.Location = new System.Drawing.Point(202, 137);
+            this.txt_apellido.Name = "txt_apellido";
+            this.txt_apellido.Size = new System.Drawing.Size(88, 20);
+            this.txt_apellido.TabIndex = 10;
+            this.txt_apellido.Text = "APELLIDO";
             // 
-            // label5
+            // txt_cc
             // 
-            this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(144, 136);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(50, 13);
-            this.label5.TabIndex = 11;
-            this.label5.Text = "CEDULA";
+            this.txt_cc.AutoSize = true;
+            this.txt_cc.Location = new System.Drawing.Point(216, 209);
+            this.txt_cc.Name = "txt_cc";
+            this.txt_cc.Size = new System.Drawing.Size(75, 20);
+            this.txt_cc.TabIndex = 11;
+            this.txt_cc.Text = "CEDULA";
             // 
-            // label6
+            // txt_contraseña
             // 
-            this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(113, 268);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(81, 13);
-            this.label6.TabIndex = 12;
-            this.label6.Text = "CONTRASEÑA";
+            this.txt_contraseña.AutoSize = true;
+            this.txt_contraseña.Location = new System.Drawing.Point(170, 412);
+            this.txt_contraseña.Name = "txt_contraseña";
+            this.txt_contraseña.Size = new System.Drawing.Size(119, 20);
+            this.txt_contraseña.TabIndex = 12;
+            this.txt_contraseña.Text = "CONTRASEÑA";
             // 
             // REGISTRO
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(453, 410);
-            this.Controls.Add(this.label6);
-            this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.label2);
+            this.ClientSize = new System.Drawing.Size(680, 631);
+            this.Controls.Add(this.txt_contraseña);
+            this.Controls.Add(this.txt_cc);
+            this.Controls.Add(this.txt_apellido);
+            this.Controls.Add(this.txt_email);
+            this.Controls.Add(this.txt_tel);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.textBox4);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.button1);
-            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Controls.Add(this.txt_nombre);
+            this.Controls.Add(this.btn_Registrarse);
             this.Name = "REGISTRO";
             this.Text = "REGISTRO";
             this.ResumeLayout(false);
@@ -191,18 +177,18 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btn_Registrarse;
+        private System.Windows.Forms.TextBox txt_nombre;
         private System.Windows.Forms.TextBox textBox2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox4;
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label txt_tel;
+        private System.Windows.Forms.Label txt_email;
+        private System.Windows.Forms.Label txt_apellido;
+        private System.Windows.Forms.Label txt_cc;
+        private System.Windows.Forms.Label txt_contraseña;
     }
 }
--- biblioteca_2/REGISTRO.cs
+++ biblioteca_2/REGISTRO.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Configuration;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -17,14 +19,83 @@ namespace biblioteca_2
             InitializeComponent();
         }
 
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["conexion"].ConnectionString);
+
         private void label1_Click(object sender, EventArgs e)
         {
-            
+
         }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void btn_registro(object sender, EventArgs e)
+
         {
+            if (txt_nombre.Text == string.Empty || txt_contraseña.Text == string.Empty ||
+                txt_apellido.Text == string.Empty || txt_tel.Text == string.Empty ||
+                txt_cc.Text == string.Empty || txt_email.Text == string.Empty)
+            {
+                MessageBox.Show("Debes llenar los campos");
+            }
+            else
+            {
+                bool existe = ExisteCorreo();
+                if (existe == true)
+                {
+                    MessageBox.Show("Ya existe un correo con ese nombre");
+                    conn.Close();
+                }
+                else
+                {
+                    try
+                    {
+                        string query = "INSERT INTO USUARIOS(Celular,contraseña,Nombres,Apellidos,Numero_identificacion,Correo_electronico) " +
+                            "VALUES('" + txt_tel.Text + "','" + txt_contraseña.Text + "','" + txt_nombre.Text + "','" +
+                                            txt_apellido.Text + "','" + txt_cc.Text + "','" + txt_email.Text + "')";
+                        //conn.Open();
+                        SqlCommand cmd = new SqlCommand(query, conn);
+                        SqlDataAdapter da = new SqlDataAdapter(cmd);
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Correo creado correctamente");
+                        conn.Close();
+                        LimpiarCampos();
+                    }
+                    catch (Exception ex)
+                    {
 
+                        MessageBox.Show(ex.ToString());
+                    }
+                }
+
+            }
         }
+        public void LimpiarCampos()
+        {
+            txt_nombre.Text = "";
+            txt_apellido.Text = "";
+            txt_email.Text = "";
+            txt_tel.Text = "";
+            txt_cc.Text = "";
+            txt_contraseña.Text = "";
+        }
+        public bool ExisteCorreo()
+        {
+            bool existe = false;
+            conn.Open();
+            string query = "select COUNT(*) from USUARIOS where Correo_electronico = @Correo_electronico";
+            SqlCommand consulta = new SqlCommand(query, conn);
+            consulta.Parameters.AddWithValue("@Correo_electronico", txt_email.Text);
+
+            int count = Convert.ToInt32(consulta.ExecuteScalar());
+            if (count > 0)
+            {
+                existe = true;
+            }
+            else
+            {
+                existe = false;
+            }
+
+            return existe;
+        }
+
     }
 }
